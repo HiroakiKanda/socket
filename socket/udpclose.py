3d61c28a1578d1640027375b81f0bcb8d6e45ed9
H.KANDA <iiskanda@IIS-MacBook.local>
2019-08-19 10:47:04 +0900

    20190819-01

---
+++ socket/udpclose.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+import time
+from socket import socket, AF_INET, SOCK_DGRAM
+
+HOST = ''
+PORT = 8978
+#ADDRESS = "172.31.92.113" # 自分に送信
+ADDRESS = "127.0.0.1" # 自分に送信
+
+s = socket(AF_INET, SOCK_DGRAM)
+# ブロードキャストする場合は、ADDRESSを
+# ブロードキャスト用に設定して、以下のコメントを外す
+# s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+while True:
+
+    msg = "close"
+    # 送信
+    s.sendto(msg.encode(), (ADDRESS, PORT))
+    time.sleep(3)
+
+s.close()
